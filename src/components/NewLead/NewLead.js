10f2107dc2823ad174278d31775da71b69d9d9bc
Abdul Azim <azeemabdulshaikh@gmail.com>
2021-08-06 18:49:00 +0530

    Contact form

--- src/components/NewLead/NewLead.js
+++ src/components/NewLead/NewLead.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './NewLead.css'
-import { Row, Col,Form,Typography,Button} from 'antd';
+import { Row, Col,Form,Typography,Button,Input,Select} from 'antd';
 const { Title } = Typography;
+
+const { Option } = Select;
+const formItemLayout = {
+    labelCol: {
+      span: 24,
+    },
+    wrapperCol: {
+      span: 24,
+    },
+  };
 const NewLead = () => {
+
     return (
         <div className="form-container">
             <Row gutter={[40,24]} justify="center">
@@ -11,9 +22,161 @@ const NewLead = () => {
                         <Col className="contact-details" xs={22} sm={24} md={24} lg={24} xl={24}  span={24}>
                             <div className="form-title">
                                 <Title level={4}>Contact Details</Title>
-                                <Form >
-                                </Form>
                             </div>
+                            <Form layout="horizontal" className="contact-detail-form">
+                                <Col >
+                                    <Form.Item
+                                        className="form-item-name"
+                                        {...formItemLayout}
+                                        name={['user', 'name']}
+                                        label="First Name"
+                                        rules={[
+                                        {
+                                            required: true,
+                                        },
+                                        ]}
+                                    >
+                                        <Input className="first-name border-bottom" placeholder="Enter First Name"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name={['user', 'name']}
+                                        label="Surname"
+                                        rules={[
+                                        {
+                                            required: true,
+                                        },
+                                        ]}
+                                    >
+                                        <Input className="last-name border-bottom"  placeholder="Enter Surname"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name={['user', 'name']}
+                                        label="Email Address "
+                                        rules={[
+                                        {
+                                            required: false,
+                                        },
+                                        ]}
+                                    >
+                                        <Input className="email border-bottom" placeholder="Enter Email Address"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                    {...formItemLayout}
+                                    name={['user', 'name']}
+                                    label="Primary Mobile"
+                                    rules={[
+                                    {
+                                        required: true,
+                                    },
+                                    ]}
+                                    >
+                                        <Input className="phone-no border-bottom" placeholder="Enter Primary Mobile"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name="State"
+                                        label="State"
+                                        hasFeedback
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'Select your State!',
+                                        },
+                                        ]}
+                                    >
+                                        <Select placeholder="Select a country">
+                                        <Option value="china">China</Option>
+                                        <Option value="usa">U.S.A</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name="City"
+                                        label="City"
+                                        hasFeedback
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please select your city!',
+                                        },
+                                        ]}
+                                    >
+                                        <Select placeholder="Select a city">
+                                        <Option value="china">China</Option>
+                                        <Option value="usa">U.S.A</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name="Lead Type"
+                                        label="Lead Type"
+                                        hasFeedback
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'Select Lead Type',
+                                        },
+                                        ]}
+                                        >
+                                        <Select placeholder="New Bussiness">
+                                        <Option value="china">China</Option>
+                                        <Option value="usa">U.S.A</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                    {...formItemLayout}
+                                    name="Product"
+                                    label="Product"
+                                    hasFeedback
+                                    rules={[
+                                    {
+                                        required: true,
+                                        message: 'Select Product',
+                                    },
+                                    ]}
+                                    >
+                                        <Select placeholder="Select Product">
+                                        <Option value="china">China</Option>
+                                        <Option value="usa">U.S.A</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                                <Col >
+                                    <Form.Item
+                                        {...formItemLayout}
+                                        name="Insurance Company"
+                                        label="Insurance Company"
+                                        hasFeedback
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'Insurance Company',
+                                        },
+                                        ]}
+                                    >
+                                        <Select placeholder="Insurance">
+                                        <Option value="china">China</Option>
+                                        <Option value="usa">U.S.A</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                            </Form>
                         </Col>
                         <Col className="status" xs={22} sm={24} md={24} lg={24} xl={24}  span={24}>
                             <div className="form-title">
