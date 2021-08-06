a295016c005d6dcb14b7e37f862041aadff8aeed
Siddhesh Bakshi <siddhesh.bakashi@gmail.com>
2021-08-06 10:32:16 +0530

    rewards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Login from './components/Login';
+
+import Achievment from './components/Achievement';
+import ActiveContest from './components/ActiveContest';
+import AllContest from './components/AllContest';
+import AllContestDetails from './components/AllContestDetails';
+import ActiveContestDetails from './components/ActiveContestDetails';
+import CompletedContest from './components/CompletedContest';
 function App() {
   return (
     <div>
-      <Login/>
+      <ActiveContestDetails />
     </div>
   );
 }
