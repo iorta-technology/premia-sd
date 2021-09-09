25cb55c0f7f92125f30862900c13e9fc5b5009e3
Abdul Azim <azeemabdulshaikh@gmail.com>
2021-09-09 16:19:50 +0530

    dynamic lead form

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import leadsReducer from './leads';
+import addressReducer from './address'
 import loginReducer from './auth';
 import homeReducer from './home';
 import activitiesReducer from './home'
 const rootReducer = combineReducers({
     leads:leadsReducer,
+    address:addressReducer,
     login:loginReducer,
     home: homeReducer,
     activities: activitiesReducer
