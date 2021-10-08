9745a28ec0faf0e4bce13891eeca6ce60fa5323a
Abdul Azim <azeemabdulshaikh@gmail.com>
2021-10-08 20:09:55 +0530

    history component ui updated

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Row, Col } from 'antd';
+import { Row, Col,Steps,Divider } from 'antd';
+
 import './History.css'
 import '../StatusLead/StatusLead.css'
 import Tabs from '../Tab/Tab'
 import HistoryTabs from './HistoryTabs'
+const { Step } = Steps;
+
 
 let historyRoute = "/leadmasterpage/leadhistorymaster/leadhistory"
 const tabMenu = [
@@ -45,12 +48,29 @@ const History = () => {
                     <Col xs={24} sm={22} md={4} offset={2}>
                         <HistoryTabs  />
                     </Col>
-                    <Col className="form-body m0a" xs={22} sm={24} md={16} lg={16} xl={16} >
+                    {/* <Col className="form-body m0a" xs={22} sm={24} md={16} lg={16} xl={16} >
                         <div className="proposal">
                             <div className="bg-norecord">
                             </div>
                             <p className="norecord-title">No Records Found</p>
                         </div>
+                    </Col> */}
+                    <Col xs={22} sm={22} md={16} offset={4} className="form-body m0a his-container">
+                        <h2 className="his-title m0a">Lead Data</h2>
+                        <Steps progressDot current={1} direction="vertical"   className="p40" >
+                            <Step title="Finished" description="This is a description." />
+                            <Step title="In Progress" description="This is a description." />
+                        </Steps>
+                        <h2 className="his-title m0a">Lead Data</h2>
+                        <Steps progressDot current={1} direction="vertical"  className="p40">
+                            <Step title="Finished" description="This is a description." />
+                            <Step title="In Progress" description="This is a description." />
+                        </Steps>
+                        <h2 className="his-title m0a">Lead Data</h2>
+                        <Steps progressDot current={1} direction="vertical"  className="p40">
+                            <Step title="Finished" description="This is a description." />
+                            <Step title="In Progress" description="This is a description." />
+                        </Steps>
                     </Col>
                 </Row>
             </div>
