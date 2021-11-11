ade2b60bf06c8a0752e3388fb66d6a3c34b708a4
shivakumarrontala1 <shiva.kumar@iorta.in>
2021-11-11 09:48:11 +0530

    changes of renewalmaster

--- src/components/RenewalCollections/Cards/RenewalCards.js
+++ src/components/RenewalCollections/Cards/RenewalCards.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Cards from './Cards'
 import './RenewalCards.css'
+import { Row, Col,Card } from 'antd'
 import _ from "lodash";
+import NoRecordsFound from '../../NoRcordsFound/NoRecordsFound';
 
 const RenewalCards = (props)=> {
     let card = [];
+    if(_.isEmpty(props.renewals)){return <NoRecordsFound/>    }
     if(props.renewals && !_.isEmpty(props.renewals)){
         card = _.map(props.renewals, (user, index) => {
             console.log("cards",user)
-       return ( <React.Fragment>
+       return ( <>
+           <Col sm={18}  md={18} lg={11} xl={10}>
             <Cards 
                 key={user._id}
                 user_Id={user.proposer_ID}
@@ -19,13 +23,18 @@ const RenewalCards = (props)=> {
                 proposer_ID_refs={user.proposer_ID_refs}
                 loading={props.renewalsDataLoading}
             />
-        </React.Fragment>
+            </Col>
+        </>
        )
         })
     }
     return (
             <div className="cards-container">
+                <Row justify="center" gutter={[18,{ xs: 8, sm: 10, md: 10, lg:18 }]}>
               {card}
+              <Col sm={18}  md={18} lg={11} xl={10}>
+                    </Col>
+              </Row>
             </div>
     )
 }
