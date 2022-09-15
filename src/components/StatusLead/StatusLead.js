e722858f3de0438558d3410328d579b8a0fa5f9d
User <user@Users-MacBook-Pro.local>
2022-09-15 11:27:11 +0530

    fixed the issues

--- src/components/StatusLead/StatusLead.js
+++ src/components/StatusLead/StatusLead.js
@@ -317,9 +317,9 @@ const NewLead = React.memo(() => {
 
 
   let leadDataLoading = useSelector((state) => state.newLead.leadDataloading)
-  let payloadFormData = useSelector((state) => state.newLead.payloadFormData)
+  // let payloadFormData = useSelector((state) => state.newLead.payloadFormData)
   const storeLeadId = useSelector((state) => state.newLead.leadId)
-  const leadDataloading = useSelector((state) => state.newLead.leadDataloading)
+  // const leadDataloading = useSelector((state) => state.newLead.leadDataloading)
   const storefirstNameValue = useSelector((state) => state.newLead.formData.firstName)
   const storelastNameValue = useSelector((state) => state.newLead.formData.lastName)
   const storeEmailValue = useSelector((state) => state.newLead.formData.email)
@@ -327,16 +327,18 @@ const NewLead = React.memo(() => {
   const storeStateValue = useSelector((state) => state.newLead.formData.state)
   const storeCityValue = useSelector((state) => state.newLead.formData.city)
   const storeLeadTypeValue = useSelector((state) => state.newLead.formData.LeadType)
+  console.warn('((((((((((( storefirstNameValue )))))))))))',storefirstNameValue)
   const storeProductValue = useSelector((state) => state.newLead.formData.Product)
   const storeInsuranceCompanyValue = useSelector((state) => state.newLead.formData.Insurance_Company)
   const storeLeadStatusValue = useSelector((state) => state.newLead.formData.leadStatus)
   const storeLeadDispositionValue = useSelector((state) => state.newLead.formData.leadDisposition)
   const storeLeadSubDispositionValue = useSelector((state) => state.newLead.formData.leadDisposition)
+  const storeLeadStatsArr = useSelector((state) => state.newLead.formData.leadStatusArr)
   const start_date = useSelector((state) => state.newLead.formData.start_date)
   const start_time = useSelector((state) => state.newLead.formData.start_time)
   // const {start_date,start_time} = storeAppointmentData
   // const storeReminderValue = useSelector((state)=>state.newLead.formData.reminder)
-  console.log(typeof (start_time))
+  // console.log(typeof (start_time))
   // console.warn('((((((((((( storeFormData )))))))))))',storeFormData)
   // msToDateString(1637605800000)  
   const storeRemarkFromSourceValue = useSelector((state) => state.newLead.formData.remarksfromSource)
@@ -384,94 +386,80 @@ const NewLead = React.memo(() => {
   // parseInt(start_time)
   const [remarkFromSource, setRemarkFromSource] = useState(storeRemarkFromSourceValue)
   const [remarkFromUser, setRemarkFromUser] = useState(storeRemarkFromUserValue)
-  // const [leadType, setLeadType] = useState(storeLeadTypeValue)
-  const [leadType, setLeadType] = useState('select')
-  // const [product, setProduct] = useState(storeProductValue)
-  const [product, setProduct] = useState('select')
-  // const [insuranceCompany, setInsuranceComapany] = useState(storeInsuranceCompanyValue)
-  const [insuranceCompany, setInsuranceComapany] = useState('select')
-  // const [stateProvince, setStateProvince] = useState(storeStateValue)
-  const [stateProvince, setStateProvince] = useState('Select')
-  // const [cityProvince, setCityProvince] = useState(storeCityValue)
-  const [cityProvince, setCityProvince] = useState('Select')
+  const [leadType, setLeadType] = useState(storeLeadTypeValue !== '' ? storeLeadTypeValue : 'select')
+  // const [leadType, setLeadType] = useState('select')
+  const [product, setProduct] = useState(storeProductValue !== '' ? storeProductValue : 'select')
+  // const [product, setProduct] = useState('select')
+  const [insuranceCompany, setInsuranceComapany] = useState(storeInsuranceCompanyValue !== '' ? storeInsuranceCompanyValue : 'select')
+  // const [insuranceCompany, setInsuranceComapany] = useState('select')
+  const [stateProvince, setStateProvince] = useState(storeStateValue !== '' ? storeStateValue : 'Select')
+  // const [stateProvince, setStateProvince] = useState('Select')
+  const [cityProvince, setCityProvince] = useState(storeCityValue !== '' ? storeCityValue : 'Select')
+  // const [cityProvince, setCityProvince] = useState('Select')
   const [errorMessage, setErrorMessage] = useState()
   const [isNewLead, setIsNewLead] = useState(false)
-  const [leadStatusData, setLeadStatusData] = useState([])
+  const [leadStatusData, setLeadStatusData] = useState(storeLeadStatsArr)
   const [hierarAgentList ,setHierarAgentList]=useState([])
   const [desigData ,setDesigData]=useState('')
   const [teamMemberList ,setTeamMemberList]=useState([])
   const [teamData ,setTeamData]=useState('')
 
   // useEffect(() => {
-  //   // setLeadStatusData(leadArr)
-  //   setLeadStatusData([...leadStatusData,leadArr])
-  //   console.warn('leadStatusData((((((((((===>>>>>>>>>>', leadStatusData)
-  // }, [leadArr])
-
+  //   // if (storeFormData.lead_Id !== '') {
+  //   //   console.warn('UPDATE___DATAAAA((((((((((===>>>>>>>>>>', storeFormData)
+  //   //   setFirstName(storefirstNameValue)
+  //   // }
+  //   console.warn('storefirstNameValue((((((((((===>>>>>>>>>>', storefirstNameValue)
+  //   // console.warn('lastName((((((((((===>>>>>>>>>>', lastName)
+  //   // console.warn('email((((((((((===>>>>>>>>>>', email)
+  //   // console.warn('primaryNo((((((((((===>>>>>>>>>>', primaryNo)
+  //   setFirstName(storefirstNameValue)
+  //   // setLastName(storelastNameValue)
+  //   // setLeadStatus(storeEmailValue)
+  //   // setPrimaryNo(storePrimaryMobileValue)
+  //   // setLeadType(storeLeadTypeValue)
+  // },[storefirstNameValue])
 
   useEffect(() => {
-    if (storeFormData.lead_Id !== '') {
-      // let _status = []
-      console.warn('UPDATE', storeFormData)
-      // console.warn('storeFormData.leadStatusArr((((((((((===>>>>>>>>>>', storeFormData.leadStatusArr)
-      if(storeFormData.leadStatusArr > 0 || storeFormData.leadStatusArr !== undefined) setLeadStatusData(storeFormData.leadStatusArr)
-      setFirstName(storeFormData.firstName)
-      setLastName(storeFormData.lastName)
-      setLeadStatus(storeFormData.leadStatus)
-      setPrimaryNo(storeFormData.primaryMobile)
-      setLeadType(storeFormData.leadType)
+    // if (storeFormData.lead_Id !== '') {
+    //   // let _status = []
+    //   console.warn('UPDATE', storeFormData)
+    //   // console.warn('storeFormData.leadStatusArr((((((((((===>>>>>>>>>>', storeFormData.leadStatusArr)
+    //   // if(storeFormData.leadStatusArr > 0 || storeFormData.leadStatusArr !== undefined) setLeadStatusData(storeFormData.leadStatusArr)
+    //   setFirstName(storeFormData.firstName)
+    //   setLastName(storeFormData.lastName)
+    //   setLeadStatus(storeFormData.leadStatus)
+    //   setPrimaryNo(storeFormData.primaryMobile)
+    //   setLeadType(storeFormData.leadType)
 
-      setStateProvince(storeFormData.state)
-      setCityProvince(storeFormData.city)
-      setEmail(storeFormData.email)
-      setInsuranceComapany(storeFormData.Insurance_Company)
-      setProduct(storeFormData.Product)
+    //   setStateProvince(storeFormData.state)
+    //   setCityProvince(storeFormData.city)
+    //   setEmail(storeFormData.email)
+    //   setInsuranceComapany(storeFormData.Insurance_Company)
+    //   setProduct(storeFormData.Product)
       
-      // leadHandler(storeFormData.leadStatusArr)
-      
-      // setLeadStatusData(storeFormData.leadStatusArr)
-      // setLeadStatusData([...leadStatusData,storeFormData.leadStatusArr])
-      // if(leadArr.length > 0) setLeadStatusData([...leadStatusData,leadArr])
-
-      // console.warn('leadStatusData((((((((((===>>>>>>>>>>', leadStatusData)
-
-      // storeFormData.leadStatus !== '' && _status.push(storeFormData.leadStatus)
-      // storeFormData.leadDisposition !== '' && _status.push(storeFormData.leadDisposition)
-      // storeFormData.leadsubDisposition !== '' && _status.push(storeFormData.leadsubDisposition)
-
-      // let _status = [storeFormData.leadStatus , storeFormData.leadDisposition , storeFormData.leadsubDisposition]
-      // console.warn('_status((((((((((===>>>>>>>>>>', _status)
-
-    } else {
-      console.warn('CREATE', storeFormData.lead_Id)
-      // setLeadType('select')
-      // setStateProvince('Select')
-      // setCityProvince('Select')
-      // setInsuranceComapany('select')
-      // setProduct('select')
-    }
+    //   // leadHandler(storeFormData.leadStatusArr)
+    //   // setLeadStatusData(storeFormData.leadStatusArr)
+    //   // setLeadStatusData([...leadStatusData,storeFormData.leadStatusArr])
+    // } else {
+    //   console.warn('CREATE', storeFormData.lead_Id)
+    // }
 
     // console.warn('userTreeData((((((((((===>>>>>>>>>>', userTreeData)
+    if(userTreeData.length > 0){
+      userTreeData.reporting_hierarchies.forEach(el =>{ el.label = el.dispValue })
+      userTreeData.reporting_users.forEach(el =>{ 
+        el.label = el.full_name
+        el.value = el._id 
+      })
+      setHierarAgentList(userTreeData.reporting_hierarchies)
+    }
+    
 
-    userTreeData.reporting_hierarchies.forEach(el =>{ el.label = el.dispValue })
-    userTreeData.reporting_users.forEach(el =>{ 
-      el.label = el.full_name
-      el.value = el._id 
-    })
-    setHierarAgentList(userTreeData.reporting_hierarchies)
-
-    // console.log('payload',payloadFormData)
-    // console.log('nonpayload',storeFormData)
     // dispatch(actions.fetchLeadDetails(fetchLeadId))
-    // console.log(leadDisposition === "appointment" || leadDisposition === "callback" || !appointmentStatus)
-    // console.log(appointmentStatus)
-
-    // console.log('storeLeadId=======>>>', storeLeadId)
-    storeLeadId !== '' ? setIsNewLead(false) : setIsNewLead(true)
+    storeFormData.lead_Id !== '' ? setIsNewLead(false) : setIsNewLead(true)
     primaryNo.length === 10 ? setmobileNoValid(true) : setmobileNoValid(false)
 
-    // console.log(_.isEmpty(storeAppointmentData))
-    // console.log(storeAppointmentData)
     // form.resetFields({
     //   "firstname":firstName,
     //   "lastname":lastName,
@@ -486,21 +474,21 @@ const NewLead = React.memo(() => {
     //   "remarksfromsource":remarkFromSource,
     //   "remarksfromuser":remarkFromUser,
     // })
-    form.setFieldsValue({
-      "firstname": firstName,
-      "lastname": lastName,
-      "email": email,
-      "phone": primaryNo,
-      "state": stateProvince,
-      "city": cityProvince,
-      "leadType": leadType,
-      "product": product,
-      "insuranceCompany": insuranceCompany,
-      "appointmentDate": appointmentDate,
-      "remarksfromsource": remarkFromSource,
-      "remarksfromuser": remarkFromUser,
-      // "leadStatus":leadSubDisposition
-    })
+    // form.setFieldsValue({
+    //   "firstname": firstName,
+    //   "lastname": lastName,
+    //   "email": email,
+    //   "phone": primaryNo,
+    //   "state": stateProvince,
+    //   "city": cityProvince,
+    //   "leadType": leadType,
+    //   "product": product,
+    //   "insuranceCompany": insuranceCompany,
+    //   "appointmentDate": appointmentDate,
+    //   "remarksfromsource": remarkFromSource,
+    //   "remarksfromuser": remarkFromUser,
+    //   // "leadStatus":leadSubDisposition
+    // })
   }, [
     firstName,
     lastName,
@@ -523,10 +511,11 @@ const NewLead = React.memo(() => {
     storePrimaryMobileValue,
     stateProvince,
     leadStatusData,
-    leadDataloading,
+    // leadDataloading,
     storeCityValue,
     storeStateValue
   ])
+  
   // add team Member modal state control
   const [visibleTeamMemberModal, setVisibleTeamMemberModal] = useState(false);
 
@@ -739,6 +728,7 @@ const NewLead = React.memo(() => {
   ];
 
   const onChangeFirstName = (e) => {
+    // console.warn('FIRSTNAME',e)
     setFirstName(e.target.value)
   }
 
@@ -1294,12 +1284,12 @@ const NewLead = React.memo(() => {
         <Form
           layout="horizontal"
           className="contact-detail-form"
-          validateMessages={validateMessages}
+          // validateMessages={validateMessages}
           scrollToFirstError
-          form={form}
-          help={errorMessage}
+          // form={form}
+          // help={errorMessage}
           onFinish={submitHandler}
-          onFinishFailedFucn={failedHandler}
+          // onFinishFailedFucn={failedHandler}
           initialValues={{
             "firstname": firstName,
             "lastname": lastName,
@@ -1319,7 +1309,7 @@ const NewLead = React.memo(() => {
             "leadStatus": leadStatusData,
             "appointmentStatus": ['newappointment', 'newApptmnt']
           }}
-          onFinishFailed={onFinishFailedFucn}
+          // onFinishFailed={onFinishFailedFucn}
         >
           <Row justify={width > breakpoint ? "" : "center"} gutter={[0, 24]}  >
             <Col className="form-body  p50 mb-2" xs={24} sm={24} md={16} lg={15} xl={15} span={23} offset={width > breakpoint ? 2 : 0}>
@@ -1348,8 +1338,8 @@ const NewLead = React.memo(() => {
                       className="first-name input-box "
                       size="large"
                       placeholder="Enter First Name"
-                      // value={firstName}
-                      defaultValue={firstName}
+                      value={firstName}
+                      // defaultValue={firstName}
                       onChange={(item)=>onChangeFirstName(item)}
                     />
                   </Form.Item>
@@ -1413,14 +1403,18 @@ const NewLead = React.memo(() => {
                         required: true,
                         message: 'Mobile No is required'
                       },
-                      () => ({
-                        validator(_, value) {
-                          if (value.length !== 10) {
-                            return Promise.reject("Number must be 10 digits");
-                          }
-                          return Promise.resolve();
-                        },
-                      }),
+                      {
+                        message: 'Number must be 10 digits',
+                        pattern: new RegExp("^[6-9][0-9]{9}$")
+                      }
+                      // () => ({
+                      //   validator(_, value) {
+                      //     if (value.length !== 10) {
+                      //       return Promise.reject("Number must be 10 digitsAA");
+                      //     }
+                      //     return Promise.resolve();
+                      //   },
+                      // }),
                       // !mobileNoCheck || {
                       //   // type:'phone',
                       //   min: 10,
@@ -1999,7 +1993,7 @@ const NewLead = React.memo(() => {
             </Col>
           </Row>
         </Form>
-        <FloatButton />
+        {/* <FloatButton /> */}
       </div>
 
     </>
