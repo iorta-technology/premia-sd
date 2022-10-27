088312c7ddd083c5364d33dc39326450b69f3179
Shubham Upadhyay <shubham.upadhyay@iorta.in>
2022-10-27 18:33:41 +0530

    changes

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -67,6 +67,7 @@ const logoutStart = (state, action) => {
 };
 
 const logoutSuccess = (state, action) => {
+
   stoageSetter("user", null);
   stoageSetter("headers", {});
   window.localStorage.removeItem("user");
