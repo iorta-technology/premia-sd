e5add4a55fb4e0cc2ee8e826265ff4353a3f229d
Sadique <mohammad.sadiqu@iorta.in>
2023-02-27 20:26:10 +0530

    file upload

--- src/components/CompanyIntelligence/DocumentUpload.js
+++ src/components/CompanyIntelligence/DocumentUpload.js
@@ -11,6 +11,7 @@ import {
   Select,
   Button,
   Upload,
+  message,
 } from "antd";
 import "../StatusLead/StatusLead.css";
 import * as actions from "../../store/actions/index";
@@ -25,6 +26,7 @@ import {
 } from "@ant-design/icons";
 import rootIndex from "../../store/root_index";
 import apiConfig from "../../config/api.config";
+import { shadows } from "@mui/system";
 
 const { store } = rootIndex;
 const { baseURL, auth, secure, NODE_ENV } = apiConfig;
@@ -54,7 +56,7 @@ const DocUpload = (props) => {
 
   const [ourAskData, setOurAskData] = useState("");
   const [redFlagData, setRedFlagData] = useState("");
-  const [fileList, setFileList] = useState([]);
+  const [documentType, setDocumentType] = useState([]);
   const [fileData, setFileData] = useState([]);
   const [docData, setdocData] = useState([]);
   const [clientExpectationData, setClientExpectationData] = useState("");
@@ -83,13 +85,6 @@ const DocUpload = (props) => {
     },
   };
 
-  useEffect(() => {
-    console.log(props.leadDetails, "lead id----->");
-    console.log(_StoreData.documents, "useefeects documents");
-    setFileData(_StoreData.documents);
-    setdocData(_StoreData.documents);
-  }, []);
-
   const kdmRoleArr = [{ label: "TBI", value: "TBI" }];
 
   const onChangeOurAsk = (e) => {
@@ -108,114 +103,112 @@ const DocUpload = (props) => {
 
   const handleChangeFile = async (info) => {
     const _store = store.getState();
-    console.log(info.file.originFileObj, "info file---->");
+    console.log(info[0], "info file---->");
     let newArr = [...fileData];
     let documentType = "PDF";
 
-    // Update the formData object
+    const formData = new FormData();
+    formData.append("media_upload", info[0]);
 
-    // let axiosConfig = {
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data',
-    //     authorization: "Bearer " + _store.login.token
-    //   },
-    // }
-    // axios
-    // .post(
-    //     "https://b2bnodedev.salesdrive.app/b2b/secure/admin/v2/uploadFile",
-    //     {media_upload : info.file},
-    //     axiosConfig
-    // )
-    // .then((res) => {
-    //   console.log(res,'fina res of upload------->');
-    // });
-    // catch (err) {
-    //   if (DocumentPicker.isCancel(err)) {
-    //     console.log("error -----", err);
-    //   } else {
-    //     throw err;
-    //   }
-    // }
+    let axiosConfig = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        authorization: "Bearer " + _store.login.token,
+      },
+    };
 
-    newArr.push(info.file.originFileObj);
-    setFileData(newArr);
-    console.log("fileList--->", newArr);
+    axios
+      .post(
+        "https://b2bnodedev.salesdrive.app/b2b/secure/admin/v2/uploadFile",
+        formData,
+        axiosConfig
+      )
+      .then((res) => {
+        console.log("data", res.data.errMsg);
+        newArr.push({ ...res.data.errMsg, recent: true });
+        setFileData(newArr);
+        console.log("fileList--->", newArr);
+      })
+      .catch((err) => {
+        console.log("error -----", err);
+        throw err;
+      });
   };
 
   const Save = async () => {
     var finalData = [];
-    if (fileData.length != 0) {
-      fileData?.map(async (item, index) => {
-        let formData = new FormData();
-        formData.append("media_upload", item);
-        console.log(Object.fromEntries(formData), "formdata------->");
-        // Details of the uploaded file
 
-        let result = await axiosRequest.post(`admin/v2/uploadFile`, formData, {
-          secure: true,
-          multipart: true,
-        });
-        console.log(result.location, "upload result--====>");
-        let obj = {
-          url: result.location,
-          doc_category: "Photo ID, Aadhar Card",
-        };
-        finalData.push(obj);
-        console.log(finalData, "final data before all subit----->");
-        if (
-          index == fileData.length - 1 &&
-          finalData.length == fileData.length
-        ) {
-          setdocData(finalData);
-          let formBody = {
-            company_details: {
-              company_name: _StoreData?.company_id?.company_name,
-              parent_company: _StoreData?.company_id?.parent_company,
-              industry_name: _StoreData?.company_id?.industry_name,
-              tata_aig_empaneled:
-                _StoreData?.company_id?.tata_aig_empaneled === true
-                  ? "Yes"
-                  : "No",
-              client_location: _StoreData?.company_id?.client_location,
-            },
-            leadStatus: _StoreData?.leadStatus,
-            leadDisposition: _StoreData?.leadDisposition,
-            leadsubDisposition: _StoreData?.leadsubDisposition,
-            opportunity_name: _StoreData?.opportunity_name,
-            tender_driven: _StoreData?.tender_driven === true ? "Yes" : "No",
-            LOB_opportunity: _StoreData?.lob_for_opportunity,
-            product_for_opportunity: _StoreData?.product_for_opportunity,
-            // remarks: _StoreData?.remarks,
-            teamMembers: "[]",
-            lead_Owner_Id: user_id,
-            lead_Creator_Id: user_id,
-            user_id: user_id,
-            company_id: _StoreData?.company_id?._id,
-            start_date: _StoreData?.start_date,
-            start_time: _StoreData?.start_time,
-            client_expectations: _StoreData?.client_expectations,
-            red_flags: _StoreData?.red_flags,
-            our_ask: _StoreData?.our_ask,
-            channel_name: _StoreData?.channel_name,
-            producer: _StoreData?.producer,
-            VAS_executed: _StoreData?.VAS_executed,
-            VAS_input: _StoreData?.VAS_input,
-            kdm_details: _StoreData?.company_id?.kdm_details,
-            risk_details: _StoreData?.company_id?.risk_details,
-            documents: finalData,
+    if (fileData.length != 0 && fileData.length > 0) {
+      fileData?.map(async (item, index) => {
+        if (item.recent) {
+          console.log("item.recent=================");
+          let formData = new FormData();
+          formData.append("media_upload", item);
+          console.log("item ========== ", item);
+
+          let obj = {
+            url: item.location,
+            doc_category: "abc",
+            file_name: item.originalname,
           };
-          console.warn("formBody ------>>>>>", formBody);
-          dispatch(actions.fetchLeadUpdateBody(formBody));
-          dispatch(actions.editLead(formBody, props.leadDetails));
+          finalData.push(obj);
+          console.log(finalData, "final data before all subit----->");
         }
       });
-      console.log(finalData.length, fileData.length);
-      if (docData?.length > 0) {
-        console.log(finalData.length, fileData.length, "here-->");
-        console.log(finalData, "final data after all subit----->");
-      }
+
+      setdocData(finalData);
+      let formBody = {
+        company_details: {
+          company_name: _StoreData?.company_id?.company_name,
+          parent_company: _StoreData?.company_id?.parent_company,
+          industry_name: _StoreData?.company_id?.industry_name,
+          tata_aig_empaneled:
+            _StoreData?.company_id?.tata_aig_empaneled === true ? "Yes" : "No",
+          client_location: _StoreData?.company_id?.client_location,
+        },
+        leadStatus: _StoreData?.leadStatus,
+        leadDisposition: _StoreData?.leadDisposition,
+        leadsubDisposition: _StoreData?.leadsubDisposition,
+        opportunity_name: _StoreData?.opportunity_name,
+        tender_driven: _StoreData?.tender_driven === true ? "Yes" : "No",
+        LOB_opportunity: _StoreData?.lob_for_opportunity,
+        product_for_opportunity: _StoreData?.product_for_opportunity,
+        // remarks: _StoreData?.remarks,
+        teamMembers: "[]",
+        lead_Owner_Id: user_id,
+        lead_Creator_Id: user_id,
+        user_id: user_id,
+        company_id: _StoreData?.company_id?._id,
+        start_date: _StoreData?.start_date,
+        start_time: _StoreData?.start_time,
+        client_expectations: _StoreData?.client_expectations,
+        red_flags: _StoreData?.red_flags,
+        our_ask: _StoreData?.our_ask,
+        channel_name: _StoreData?.channel_name,
+        producer: _StoreData?.producer,
+        VAS_executed: !_StoreData?.VAS_executed
+          ? "Yes"
+          : _StoreData?.VAS_executed,
+        VAS_input: _StoreData?.VAS_input,
+        kdm_details: _StoreData?.company_id?.kdm_details,
+        risk_details: _StoreData?.company_id?.risk_details,
+        documents: finalData,
+      };
+      console.warn("formBody ------>>>>>", formBody);
+      dispatch(actions.fetchLeadUpdateBody(formBody));
+      dispatch(actions.editLead(formBody, props.leadDetails));
+    } else {
+      message.info("Please Upload the new File");
     }
   };
+
+  useEffect(() => {
+    console.log(props.leadDetails, "lead id----->");
+    var newArr = _StoreData.documents.map((res) => ({ ...res, recent: false }));
+    console.log("newArr =========== ", _StoreData);
+    setFileData(newArr);
+    setdocData(_StoreData.documents);
+  }, [_StoreData.documents]);
 
   const delDoc = (i) => {
     var newArr = [...fileData];
@@ -268,8 +261,26 @@ const DocUpload = (props) => {
               label="Upload"
               style={{ marginBottom: "1rem" }}
             >
-              {/* <input type='file'  onChange={(e)=>handleChangeFile(e.target.files)} /> */}
-              <Upload
+              <label
+                for="upload-photo"
+                style={{
+                  border: "1px solid gray",
+                  padding: "4px 76px",
+                  color: "gray",
+                  boxShadow:
+                    "box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                }}
+              >
+                Upload File
+              </label>
+              <input
+                type="file"
+                id="upload-photo"
+                style={{ width: "100%" }}
+                accept="application/pdf,image/jpeg"
+                onChange={(e) => handleChangeFile(e.target.files)}
+              />
+              {/* <Upload
                 onChange={handleChangeFile}
                 multiple={true}
                 fileList={fileList}
@@ -287,7 +298,7 @@ const DocUpload = (props) => {
                 >
                   Upload
                 </Button>
-              </Upload>
+              </Upload> */}
             </Form.Item>
           </Col>
         </Row>
@@ -295,7 +306,17 @@ const DocUpload = (props) => {
           {fileData && fileData.length > 0 && (
             <div className="row px-3 py-2 head">
               <div className="col-2">Image</div>
-              <div className="col-6">Document Type</div>
+              <div
+                className="col-6"
+                // style={{
+                //   width: 170,
+                //   textOverflow: "ellipsis",
+                //   whiteSpace: "nowrap",
+                //   overflow: "hidden",
+                // }}
+              >
+                Document Name
+              </div>
               <div className="col-3">Action</div>
             </div>
           )}
@@ -311,7 +332,7 @@ const DocUpload = (props) => {
                 </div>
               </div>
               <div className="col-6 data flex-center">
-                Photo ID, Aadhar Card
+                {item.originalname || item.file_name}
               </div>
               <div className="col-3 flex-center">
                 <DeleteOutlined
